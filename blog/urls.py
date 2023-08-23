9b41db4e88084dfc67d08d5a62515d69d3db92b5
Rutuja <rutujashinde1126@gmail.com>
2023-08-23 10:21:19 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from blog import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,  UserPostListView
+
+#routing
+urlpatterns = [
+    path('about/', views.about,name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(),name='user-posts'),
+    path('', PostListView.as_view(),name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
+    path('post/new/', PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(),name='post-delete'),
+   # path('', views.home,name='blog-home'),
+    
+]
+  
